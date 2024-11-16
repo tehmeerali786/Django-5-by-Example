06b4adbbb48766e5163a2e71e88923da73389dd8
tehmeerali786 <freelanceali786@gmail.com>
2024-11-16 23:10:17 +0500

    Chapter 8 Orders started

--- Chapter 8/myshop/myshop/settings.py
+++ Chapter 8/myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
     'shop.apps.ShopConfig',
 
 ]
