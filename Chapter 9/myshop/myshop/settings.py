2f6645ab764cff9baf8f375de3755125d2b3190e
tehmeerali <freelanceali786@gmail.com>
2024-12-01 20:10:49 +0500

    Webhook

--- Chapter 9/myshop/myshop/settings.py
+++ Chapter 9/myshop/myshop/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 STRIPE_PUBLISHABLE_KEY = config('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 STRIPE_API_VERSION = '2024-04-10'
+STRIPE_WEBHOOK_SECRET = config('STRIPE_WEBHOOK_SECRET')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
---
+++ Chapter 9/myshop/payment/webhooks.py
@@ -0,0 +1,46 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+
+    try:
+        event = stripe.Webhook.construnct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+
+    if event.type == 'checkout.session.completed':
+        if (
+            session.mode == 'payment'
+            and session.payment_status == 'paid'
+        ):
+            try:
+                order = Order.objects.get(
+                    id = session.client_reference_id
+                )
+
+            except Order.DoesNotExist:
+                return HttpResponse(status=404)
+
+            # mark order as paid
+            order.paid = True
+            order.save()
+
+
+    return HttpResonpse(status=200)
