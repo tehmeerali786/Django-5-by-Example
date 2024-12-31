49e63909b0ce8f6d34812178d68359aaf2845a3e
tehmeerali786 <freelanceali786@gmail.com>
2025-01-01 03:07:30 +0500

    Chapter 4 Change Password

--- Chapter 4 Again/bookmarks/account/views.py
+++ Chapter 4 Again/bookmarks/account/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required 
 from django.http import HttpResponse 
 from django.shortcuts import render
 from .forms import LoginForm 
@@ -30,3 +31,12 @@ def user_login(request):
 		form = LoginForm()
 
 	return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+	return render(
+			request,
+			'account/dashboard.html',
+			{'section': 'dashboard'}
+		)
